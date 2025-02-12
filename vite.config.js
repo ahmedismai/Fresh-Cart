85df7b7dc91f5e8367f8f2def579ba7da5039514
Ahmed Ismail <ahmedismailamer@gmail.com>
2025-02-12 10:41:42 +0200

    Upload the design

--- vite.config.js
+++ vite.config.js
@@ -3,26 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Fresh-Cart/", 
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react')) return 'reactVendor';
-            if (id.includes('@tanstack/react-query') || id.includes('react-hot-toast')) return 'uiLibs';
-            return 'vendor';
-          }
-        },
-      },
-    },
-    chunkSizeWarningLimit: 700, // ✅ رفع الحد الأقصى لحجم الملفات
-    minify: 'terser', 
-    terserOptions: {
-      compress: {
-        drop_console: true, // ✅ إزالة console.log
-        drop_debugger: true, // ✅ إزالة debugger
-      },
-    },
-  },
+  
 });
