8f0586aa3f617891eae9a651a97c600e8172d7cc
Ahmed Ismail <45186751+ahmedismai@users.noreply.github.com>
2025-05-25 03:02:54 +0300

    Update Checkout.jsx

--- src/Components/Checkout/Checkout.jsx
+++ src/Components/Checkout/Checkout.jsx
@@ -15,7 +15,7 @@ export default function Checkout() {
       phone:"",
       city:"",
     },
-    onSubmit :()=> handleCheckout(cartId,`${window.location.origin}/allorders`)
+    onSubmit :()=> handleCheckout(cartId,`${window.location.origin}/#/allorders`)
   })
 
 
